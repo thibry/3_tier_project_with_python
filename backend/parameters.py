76daba6cf79f07296552d7af4d3007fd9347b5af
thibry <thethibry.training@gmail.com>
2025-04-18 17:09:26 +0200

    Requirement Parameter for frontend

---
+++ backend/parameters.py
@@ -0,0 +1,16 @@
+
+import requests
+
+
+
+master_username = "admin"
+
+db_password = "Bonaberi2022"
+
+endpoint = "database-1.cf6y2u2o0z6m.ca-central-1.rds.amazonaws.com"
+
+db_instance_name = "todo"
+
+
+if __name__ == "__main__":
+    print(master_username, db_password, endpoint, db_instance_name)
